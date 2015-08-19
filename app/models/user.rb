07b6fab3ae70ec51bed348f4baf8c5c27c07d8c8
Ravi Bakhai <ravi.bakhai@gmail.com>
2015-08-18 23:14:20 -0400

    profile migartion and associations are working

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :playlists
-  has_one :user
+  has_one :profile
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, :omniauth_providers => [:facebook]
 
