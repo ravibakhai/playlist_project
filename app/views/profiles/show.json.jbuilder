5f5b04b51c1a02d542a974fb856bdbdb0db8853e
Ravi Bakhai <ravi.bakhai@gmail.com>
2015-08-18 22:44:58 -0400

    profile migration

---
+++ app/views/profiles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @profile, :id, :name, :picture, :bio, :playlists, :user_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819020858) do
+ActiveRecord::Schema.define(version: 20150819024341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,18 @@ ActiveRecord::Schema.define(version: 20150819020858) do
   end
 
   add_index "playlists", ["user_id"], name: "index_playlists_on_user_id", using: :btree
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "picture"
+    t.string   "bio"
+    t.string   "playlists"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "reviews", force: :cascade do |t|
     t.text     "comment"
@@ -87,5 +99,6 @@ ActiveRecord::Schema.define(version: 20150819020858) do
   end
 
   add_foreign_key "playlists", "users"
+  add_foreign_key "profiles", "users"
   add_foreign_key "reviews", "playlists"
 end
